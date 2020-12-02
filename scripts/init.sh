9883bca67f6e4bbecdeddd99d5f301e4ab486557
Gregory Lindsey <gclindsey@gmail.com>
2020-12-01 18:00:45 -0800

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
 
 sudo yum update -y
-sudo amazon-linux-extras install docker
-sudo service docker start
-sudo usermod -a -G docker ec2-user
+sudo yum install git -y
+git clone https://github.com/abk7777/pyqwc.git
+
+## Install Docker
+# sudo amazon-linux-extras install docker
+# sudo service docker start
+# sudo usermod -a -G docker ec2-user
 
 # Log out and back in to acquire new permissions
+
